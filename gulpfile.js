c8eefb71fa1a6d763c5155ced111bcac26ecf87d
Alexey Mayorov <mayoorca@yandex.ru>
2023-11-25 23:07:42 +0300

    1

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,12 @@ const concat = require('gulp-concat-css');
 const plumber = require('gulp-plumber');
 const del = require('del');
 const browserSync = require('browser-sync').create();
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
+const postcc = require('gulp-postcss');
+const autoprefixer = require('autoprefixer'); 
 const mediaquery = require('postcss-combine-media-query');
 const cssnano = require('cssnano');
-const htmlMinify = require('html-minifier');
+const htmlMinify = require('html-minifier'); 
+const gulpPug = require('gulp-pug');
 
 function serve() {
   browserSync.init({
@@ -17,39 +18,48 @@ function serve() {
   });
 }
 
-function html() {
-  const options = {
-	  removeComments: true,
-	  removeRedundantAttributes: true,
-	  removeScriptTypeAttributes: true,
-	  removeStyleLinkTypeAttributes: true,
-	  sortClassName: true,
-	  useShortDoctype: true,
-	  collapseWhitespace: true,
-		minifyCSS: true,
-		keepClosingSlash: true
-	};
-  return gulp.src('src/**/*.html')
-        .pipe(plumber())
-        .on('data', function(file) {
-		      const buferFile = Buffer.from(htmlMinify.minify(file.contents.toString(), options))
-		      return file.contents = buferFile
-		    })
-				.pipe(gulp.dest('dist/'))
+function pug() {
+  return gulp.src('src/pages/**/*.pug')
+        .pipe(gulpPug({
+          pretty: true
+        })) 
+        .pipe(gulp.dest('dist/'))
         .pipe(browserSync.reload({stream: true}));
 }
 
+function html() {
+  const options = {
+    removeComments: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    sortClassName: true,
+    useShortDoctype: true,
+    collapseWhitespace: true,
+      minifyCSS: true,
+      keepClosingSlash: true
+  };
+  return gulp.src('src/**/*.html')
+  .pipe(plumber())
+          .on('data', function(file) {
+        const buferFile = Buffer.from(htmlMinify.minify(file.contents.toString(), options))
+        return file.contents = buferFile
+      })
+          .pipe(gulp.dest('dist/'))
+  .pipe(browserSync.reload({stream: true}));
+}
+
 function css() {
-  const plugins = [
+    const plugins = [
       autoprefixer(),
       mediaquery(),
       cssnano()
-  ];
-  return gulp.src('src/components/**/*.css')
+    ];
+  return gulp.src('src/blocks/**/*.css')
         .pipe(plumber())
         .pipe(concat('bundle.css'))
-        .pipe(postcss(plugins))
-				.pipe(gulp.dest('dist/'))
+        .pipe(postcc(plugins))
+				    .pipe(gulp.dest('dist/'))
         .pipe(browserSync.reload({stream: true}));
 }
 
@@ -64,15 +74,17 @@ function clean() {
 }
 
 function watchFiles() {
+  gulp.watch(['src/pages/**/*.pug'], pug);
   gulp.watch(['src/**/*.html'], html);
   gulp.watch(['src/blocks/**/*.css'], css);
   gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
 }
 
-const build = gulp.series(clean, gulp.parallel(html, css, images));
+const build = gulp.series(clean, gulp.parallel(pug, css, images));
 const watchapp = gulp.parallel(build, watchFiles, serve);
 
 exports.html = html;
+exports.pug = pug;
 exports.css = css;
 exports.images = images;
 exports.clean = clean;
